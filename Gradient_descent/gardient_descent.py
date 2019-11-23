96cd431f011add770967df9ed122ac7bc9c996cf
Jiae Lee <leejiae77@gmail.com>
2019-11-23 09:06:27 +0900

    update grad. dsc.

--- Gradient_descent/gardient_descent.py
+++ Gradient_descent/gardient_descent.py
@@ -4,24 +4,24 @@ import numpy as np
 import matplotlib.pyplot as plt
 cur = 3 # Initial value x=3
 learning_rate = 0.01 # Learning rate
-precision = 0.000001 #This tells us when to stop the algorithm
+precision = 0.000001 #Minimum
 prev_step_size = 1 #
-global_step = 1000 # maximum number of iterations
-steps = 0 #iteration counter
+global_step = 1000 # Globalsteps
+steps = 0 # count steps
 df = lambda x: 3*(x+2) #Gradient desecnt function 
 mem=np.zeros(global_step+1)
 
 while prev_step_size > precision and steps < global_step:
-    prev = cur # Record current x value in prev_x
+    prev = cur # Record current value in prev
     cur = cur - learning_rate * df(prev) # Grad descent
-    prev_step_size = abs(cur - prev) #Change in x
+    prev_step_size = abs(cur - prev) #Change in val
     steps = steps+1 # Current step
     mem[steps]=cur
-    print("Iteration",steps,"\nX value is",cur) #Print steps & current value
+    print("Step",steps,"\n value is",cur) #Print steps & current value
 
 plt.xlabel('Steps')
 plt.xlim([0, 1000])
-plt.xlabel('Current value x')   
+plt.xlabel('Current value')   
 plt.plot(mem)
 plt.show()
 
